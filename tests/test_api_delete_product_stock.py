c6abb023325347a211c0dfed3e936919c4be88a6
DrNattapoom <nattapoom.dr@gmail.com>
2023-01-29 23:10:20 +0700

    add unit tests for APIs

---
+++ tests/test_api_delete_product_stock.py
@@ -0,0 +1,50 @@
+from app import create_app
+from database.database_service import DatabaseService
+import unittest
+
+
+class TestAPIDeleteProductStock(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.database_service = DatabaseService('sqlite://')
+        self.app = create_app(self.database_service)
+        self.app.config.update({
+            'TESTING': True,
+        })
+        self.client = self.app.test_client()
+
+    def test_basic(self):
+        self.client.post("/api/vending_machines/add", data={
+            'name': 'test_vm_001',
+            'location': 'test_loc_001',
+        })
+        self.client.post("/api/product_stocks/add/1", data={
+            'prod_id': 2,
+            'stock': 100,
+        })
+        response = self.client.post("/api/product_stocks/delete/1/2")
+        assert response.status_code == 200
+
+    def test_response_json(self):
+        self.client.post("/api/vending_machines/add", data={
+            'name': 'test_vm_001',
+            'location': 'test_loc_001',
+        })
+        self.client.post("/api/product_stocks/add/1", data={
+            'prod_id': 2,
+            'stock': 100,
+        })
+        response = self.client.post("/api/product_stocks/delete/1/2")
+        response_json = response.get_json()
+        response_data = response_json['data']
+        assert response_data is None
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'product 2 is successfully deleted from vending machine 1'
+
+    def test_response_json_fail_not_exist(self):
+        response = self.client.post("/api/product_stocks/delete/1/2")
+        response_json = response.get_json()
+        response_data = response_json['data']
+        assert response_data is None
+        assert response_json['status'] == 'error'
+        assert response_json['message'] == 'unable to delete product 2 from vending machine 1'
---
+++ tests/test_api_update_vending_machine.py
@@ -0,0 +1,53 @@
+from app import create_app
+from database.database_service import DatabaseService
+import unittest
+
+
+class TestAPIUpdateVendingMachine(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.database_service = DatabaseService('sqlite://')
+        self.app = create_app(self.database_service)
+        self.app.config.update({
+            'TESTING': True,
+        })
+        self.client = self.app.test_client()
+
+    def test_basic(self):
+        self.client.post("/api/vending_machines/add", data={
+            'name': 'test_vm_001',
+            'location': 'test_loc_001',
+        })
+        response = self.client.post("/api/vending_machines/update/1", data={
+            'name': 'vm_001',
+            'location': 'loc_001',
+        })
+        assert response.status_code == 200
+
+    def test_response_json(self):
+        self.client.post("/api/vending_machines/add", data={
+            'name': 'test_vm_001',
+            'location': 'test_loc_001',
+        })
+        response = self.client.post("/api/vending_machines/update/1", data={
+            'name': 'vm_001',
+            'location': 'loc_001',
+        })
+        response_json = response.get_json()
+        response_data = response_json['data']
+        keys = ['id', 'name', 'location']
+        expected_values = [1, 'vm_001', 'loc_001']
+        assert all([response_data['post'][key] == expected_value for key, expected_value in zip(keys, expected_values)])
+        assert response_json['status'] == 'success'
+        assert response_json['message'] == 'vending machine 1 is successfully updated'
+
+    def test_response_json_fail_not_exist(self):
+        response = self.client.post("/api/vending_machines/update/1", data={
+            'name': 'vm_001',
+            'location': 'loc_001',
+        })
+        response_json = response.get_json()
+        response_data = response_json['data']
+        assert response_data['post'] == {}
+        assert response_json['status'] == 'error'
+        assert response_json['message'] == 'unable to update vending machine 1'
